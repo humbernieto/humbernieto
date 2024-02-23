b891d48ee40eab8aadf71af6aa33470fd389be5a
Humber Nieto <humbersnieto@gmail.com>
2024-02-22 22:32:09 -0500

    nextjs 14.1.0

---
+++ app/_components/Skills.tsx
@@ -0,0 +1,34 @@
+export default function Skills() {
+  return (
+    <div className="dark:bg-zinc-900 bg-zinc-50  justify-center flex mx-auto mt-12 rounded-md">
+      <pre className="text-sm text-zinc-950 dark:text-zinc-300">
+        {`
+  export function Habilidades () {
+    return (
+      <div>
+        <ul>        
+          <li>NextJs</li>
+          <li>Node.js®</li>
+          <li>TypeScript</li>
+          <li>Sanity.io</li>
+          <li>Tailwind CSS</li>
+          <li>Shopify</li>
+          <li>Git</li>
+          <li>Linux</li>
+          <li>Google Cloud</li>
+          <li>Scrum</li>
+          <li>DevOps</li>
+          <li>Firebase</li>
+          <li>MySQL</li>
+          <li>Docker</li>
+          <li>Web Accessibility</li>
+          <li>ISO 27001</li>
+        </ul>
+      </div>
+    );
+  };
+  `}
+      </pre>
+    </div>
+  );
+}
